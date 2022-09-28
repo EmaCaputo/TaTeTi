f79573ddaa7a45b3bb80442ad7b90f961276cd20
EMANUEL DAMIAN CAPUTO BLASON <38916570@comunidadort.edu.ar>
2022-09-27 21:49:35 -0300

    cambio en ganador

--- src/Components/Ganador.js
+++ src/Components/Ganador.js
@@ -1,17 +1,5 @@
 const Ganador = ({ chequeo }) => {
 
-    function mostrarGanador() {
-        var x = document.getElementById("ganador");
-        if (x.style.display === "none") {
-            x.style.display = "block";
-        } else {
-            x.style.display = "none";
-        }
-    }
-    
-
-
-
     return (
         <div>
             {
